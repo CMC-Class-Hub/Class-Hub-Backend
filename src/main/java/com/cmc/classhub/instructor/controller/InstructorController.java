6d59ddb26611f928c5090b5b53f36d0cb16e525a
ChaeEunLee00 <chaeeun6437@unist.ac.kr>
2026-01-25 14:45:00 +0900

    feat: CORS 중복 설정 제거

--- src/main/java/com/cmc/classhub/instructor/controller/InstructorController.java
+++ src/main/java/com/cmc/classhub/instructor/controller/InstructorController.java
@@ -6,7 +6,6 @@ import com.cmc.classhub.instructor.service.InstructorService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,7 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/instructors")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:3000")
 public class InstructorController {
 
     private final InstructorService instructorService;
