627f00a49b2168d1f4c2078eed1856aead634972
ChaeEunLee00 <chaeeun6437@unist.ac.kr>
2026-02-02 19:35:08 +0900

    feat: 세션, 예약 레파지토리 메서드 추가

--- src/main/java/com/cmc/classhub/reservation/repository/ReservationRepository.java
+++ src/main/java/com/cmc/classhub/reservation/repository/ReservationRepository.java
@@ -4,7 +4,12 @@ import com.cmc.classhub.reservation.domain.Reservation;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.cmc.classhub.reservation.domain.ReservationStatus;
+
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
     List<Reservation> findAllBySessionId(Long sessionId);
+
     boolean existsBySessionIdAndMemberId(Long sessionId, Long memberId);
+
+    List<Reservation> findBySessionIdAndStatus(Long sessionId, ReservationStatus status);
 }
