6241c7644e5f3271e52bc33450179c760dca6fe9
ckddlf <53569322+ckddlf@users.noreply.github.com>
2026-02-06 12:51:15 +0900

    feat: 정보수정기능 추가

--- src/main/java/com/cmc/classhub/global/auth/dto/LoginResponse.java
+++ src/main/java/com/cmc/classhub/global/auth/dto/LoginResponse.java
@@ -3,5 +3,6 @@ package com.cmc.classhub.global.auth.dto;
 public record LoginResponse(
         Long userId,
         String accessToken,
-        String name
+        String name,
+        String PhoneNumber
 ) {}
