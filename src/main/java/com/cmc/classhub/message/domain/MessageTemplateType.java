dd6922486eaa23e129d7b4959f9b65e91f8344bc
ChaeEunLee00 <chaeeun6437@unist.ac.kr>
2026-02-09 16:38:49 +0900

    feat: 알림톡 템플릿 ID 변경 (임시)

--- src/main/java/com/cmc/classhub/message/domain/MessageTemplateType.java
+++ src/main/java/com/cmc/classhub/message/domain/MessageTemplateType.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum MessageTemplateType {
-    APPLY_CONFIRMED("예약 완료 안내", "수업 예약 직후 자동으로 발송됩니다", "KA01TP260209062000182KPua00zv4Mw"),
+    APPLY_CONFIRMED("예약 완료 안내", "수업 예약 직후 자동으로 발송됩니다", "KA01TP260204085818135zn5xfzZaaNQ"),
     REMINDER_D3("D-3 리마인더", "수업 3일 전에 자동으로 발송됩니다", "KA01TP260204090209679pIynOqUgvLS"),
     REMINDER_D1("D-1 리마인더", "수업 1일 전에 자동으로 발송됩니다", "KA01TP260204090415000t4W32EUY88r");
 
